6bf329db3c95134f7fe51efd499bd60b3eecae08
Bobby Willmes <bobbywillmes@gmail.com>
2021-11-07 15:36:02 -0800

    reinitialize rails with react

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,2 @@
-class ApplicationController < ActionController::API
-  include ActionController::Cookies
-  include ActionController::RequestForgeryProtection
+class ApplicationController < ActionController::Base
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,3 @@
 Rails.application.routes.draw do
-  scope '/api' do
-    # Users
-    post '/users'       => 'users#create'
-    get '/users'        => 'users#index'
-
-     # SESSIONS
-    post '/sessions'            => 'sessions#create'
-    get  '/authenticated'       => 'sessions#authenticated'
-    delete '/sessions'          => 'sessions#destroy'
-
-      # TWEETS
-    post '/tweets'                 => 'tweets#create'
-    get  '/tweets'                 => 'tweets#index'
-    delete '/tweets/:id'           => 'tweets#destroy'
-    get  '/users/:username/tweets' => 'tweets#index_by_user'
-
-  end
-
+  get '/'    =>   'pages#main'
 end
