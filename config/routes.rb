6f09f0f1e12ca2b03c540786c5af115ab885358d
Bobby Willmes <bobbywillmes@gmail.com>
2021-11-07 16:17:46 -0800

    rails controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,22 @@
 Rails.application.routes.draw do
   get '/'    =>   'pages#main'
+
+  scope '/api' do
+    # Users
+    post '/users'       => 'users#create'
+    get '/users'        => 'users#index'
+
+     # SESSIONS
+    post '/sessions'            => 'sessions#create'
+    get  '/authenticated'       => 'sessions#authenticated'
+    delete '/sessions'          => 'sessions#destroy'
+
+      # TWEETS
+    post '/tweets'                 => 'tweets#create'
+    get  '/tweets'                 => 'tweets#index'
+    delete '/tweets/:id'           => 'tweets#destroy'
+    get  '/users/:username/tweets' => 'tweets#index_by_user'
+
+  end
+  
 end
